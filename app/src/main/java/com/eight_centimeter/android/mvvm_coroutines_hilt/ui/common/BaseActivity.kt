21c71e312e73b3dbb2205924a4e015b869c62b56
kevin.xu <xukai59740@outlook.com>
2022-07-20 11:07:47 +0800

    upgrade the library

--- app/src/main/java/com/eight_centimeter/android/mvvm_coroutines_hilt/ui/common/BaseActivity.kt
+++ app/src/main/java/com/eight_centimeter/android/mvvm_coroutines_hilt/ui/common/BaseActivity.kt
@@ -3,12 +3,19 @@ package com.eight_centimeter.android.mvvm_coroutines_hilt.ui.common
 import android.content.Context
 import android.content.res.Configuration
 import android.os.Build
+import android.os.Bundle
 import android.os.LocaleList
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.eight_centimeter.android.mvvm_coroutines_hilt.App
-import java.util.Locale
+import java.util.*
 
 abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("Activity", this::class.java.name)
+    }
 
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(newBase?.switchLanguageConfig())
--- app/src/main/java/com/eight_centimeter/android/mvvm_coroutines_hilt/ui/common/BaseFragment.kt
+++ app/src/main/java/com/eight_centimeter/android/mvvm_coroutines_hilt/ui/common/BaseFragment.kt
@@ -1,6 +1,13 @@
 package com.eight_centimeter.android.mvvm_coroutines_hilt.ui.common
 
+import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 
 abstract class BaseFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("Fragment", this::class.java.name)
+    }
 }
